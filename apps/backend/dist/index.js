d7968d5421c146f9edd78fe0f905b80b239035d2
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-10 01:04:55 +0500

    improve .env loading

--- apps/backend/dist/index.js
+++ apps/backend/dist/index.js
@@ -1,9 +1,10 @@
+console.log("STARTING INDEX.TS");
 import dotenv from "dotenv";
+dotenv.config({
+    path: "C:/WebDevPro/procom/apps/backend/.env",
+});
 import app from "./app.js";
 import { connectDB } from "./db/index.db.js";
-dotenv.config({
-    path: "./.env",
-});
 let port = process.env.PORT;
 connectDB()
     .then(() => {
