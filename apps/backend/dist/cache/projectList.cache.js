3ab6b4bd4b20a674f5058e2b769077049d49682a
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-23 03:21:31 +0500

    add projects list cache layer with redis client

---
+++ apps/backend/dist/cache/projectList.cache.js
@@ -0,0 +1,32 @@
+import { getRedis } from "../config/redis";
+let redis = getRedis();
+export const PROJECT_LIST_TTL = 60; // 60 seconds
+export const getProjectListKey = (userId) => {
+    return `project:list:${userId}`;
+};
+export const getprojectListCache = async (userId) => {
+    try {
+        return await redis.get(getProjectListKey(userId));
+    }
+    catch {
+        return null;
+    }
+};
+export const setProjectListCache = async (userId, data) => {
+    try {
+        return await redis.set(getProjectListKey(userId), JSON.stringify(data), {
+            EX: PROJECT_LIST_TTL,
+        });
+    }
+    catch {
+        return null;
+    }
+};
+export const invalidateProjectListCache = async (userId) => {
+    try {
+        return await redis.del(getProjectListKey(userId));
+    }
+    catch {
+        return null;
+    }
+};
