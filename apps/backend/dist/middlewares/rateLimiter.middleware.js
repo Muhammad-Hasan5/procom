b189ae9fe28c0c7afc795800ef320f983da7b0f6
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-25 21:48:51 +0500

    add rate limitng middleware

---
+++ apps/backend/dist/middlewares/rateLimiter.middleware.js
@@ -0,0 +1,25 @@
+import { asyncHandler } from "../utils/async-handler";
+import { ApiErrorResponse } from "../utils/api-error-response";
+import { getRedis } from "../config/redis";
+const redis = getRedis();
+const WindowTimeInterval = 300;
+const MaxRequests = 50;
+export const rateLimiter = asyncHandler(async (req, res, next) => {
+    const ip = req.ip;
+    if (!ip) {
+        return res
+            .status(400)
+            .json(new ApiErrorResponse(400, "Invalid request IP"));
+    }
+    const key = `rateLimit:${ip}`;
+    const count = await redis.incr(key);
+    if (count === 1) {
+        await redis.expire(key, WindowTimeInterval);
+    }
+    if (count > MaxRequests) {
+        return res
+            .status(429)
+            .json(new ApiErrorResponse(429, "Too many requests"));
+    }
+    next();
+});
