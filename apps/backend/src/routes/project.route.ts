3760b12517bcc72c984eef0675b84f04d4fdd214
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-25 22:08:03 +0500

    add rate limiter to project route

--- apps/backend/src/routes/project.route.ts
+++ apps/backend/src/routes/project.route.ts
@@ -13,40 +13,53 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { projectValidator } from "../validators/project.validator.js";
 import { validate } from "../middlewares/validators.middleware.js";
 import { canAccessProject } from "../middlewares/project.middleware.js";
+import { rateLimiter } from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
 // Create & get all projects of logged-in user
 router
 	.route("/projects")
-	.post(authMiddleware, projectValidator(), validate, createProject)
-	.get(authMiddleware, getUserProjects);
+	.post(
+		rateLimiter,
+		authMiddleware,
+		projectValidator(),
+		validate,
+		createProject,
+	)
+	.get(rateLimiter, authMiddleware, getUserProjects);
 
 // Get, update, delete one project
 router
 	.route("/projects/:projectId")
-	.get(authMiddleware, canAccessProject, getSingleProject)
+	.get(rateLimiter, authMiddleware, canAccessProject, getSingleProject)
 	.patch(
+		rateLimiter,
 		authMiddleware,
 		canAccessProject,
 		projectValidator(),
 		validate,
 		updateProject,
 	)
-	.delete(authMiddleware, canAccessProject, deleteProject);
+	.delete(rateLimiter, authMiddleware, canAccessProject, deleteProject);
 
 //project overview
 router
 	.route("/projects/:projectId/overview")
-	.get(authMiddleware, canAccessProject, getProjectOverview);
+	.get(rateLimiter, authMiddleware, canAccessProject, getProjectOverview);
 
 // Manage project members
 router
 	.route("/projects/:projectId/members")
-	.post(authMiddleware, canAccessProject, addMemberToProject);
+	.post(rateLimiter, authMiddleware, canAccessProject, addMemberToProject);
 
 router
 	.route("/projects/:projectId/members/:memberId")
-	.delete(authMiddleware, canAccessProject, removeMemberFromProject);
+	.delete(
+		rateLimiter,
+		authMiddleware,
+		canAccessProject,
+		removeMemberFromProject,
+	);
 
 export default router;
