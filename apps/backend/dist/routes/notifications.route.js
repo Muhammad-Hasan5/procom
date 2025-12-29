3cbb0bcb3255c47e1897d5b2707bbd1da56c3d84
Muhammad Hasan <hasanamir8901@gmail.com>
2025-12-29 05:24:30 +0500

    add notification router

---
+++ apps/backend/dist/routes/notifications.route.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { rateLimiter } from "../middlewares/rateLimiter.middleware";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getNotifications, getUnreadCount, markAllAsRead, markAsRead, } from "../controllers/notifications.controller";
+const router = Router({ mergeParams: true });
+router
+    .route("/notificaitons")
+    .get(rateLimiter, authMiddleware, getNotifications)
+    .get(rateLimiter, authMiddleware, getUnreadCount);
+router
+    .route("/notifications/mark-read/:notification-id")
+    .patch(rateLimiter, authMiddleware, markAsRead);
+router
+    .route("/notifications/mark-all-as-read")
+    .patch(rateLimiter, authMiddleware, markAllAsRead);
+export default router;
