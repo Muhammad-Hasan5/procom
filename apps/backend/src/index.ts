4284b9d679f18ee1b3bf07257f25432fb5e06fae
Muhammad Hasan <hasanamir8901@gmail.com>
2026-02-07 22:59:31 +0500

    attach server to ws

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,28 +1,32 @@
 console.log("STARTING INDEX.TS");
 
 import dotenv from "dotenv";
+import app from "./app.js";
+import { createServer } from "http";
+import { connectDB } from "./db/index.db.js";
+import { initRedis } from "./config/redis.js";
+import { initWebSocket } from "./chat/index.js";
+
 dotenv.config({
 	path: "C:/WebDevPro/procom/apps/backend/.env",
 });
 
-import app from "./app.js";
-import { connectDB } from "./db/index.db.js";
-import { initRedis } from "./config/redis.js";
+let port = Number(process.env.PORT);
 
+// redis cache
+await initRedis();
 
-let port = process.env.PORT;
+// websocket connection
+const server = createServer(app);
+initWebSocket(server);
 
+// database connection
 await connectDB()
 	.then(() => {
-		app.listen(port, () => {
+		server.listen(port, () => {
 			console.log(`Server is running on http://localhost:${port}`);
 		});
 	})
 	.catch((error) => {
 		console.error("Something went wrong with Database", error);
 	});
-
-await initRedis()
-
-
-
