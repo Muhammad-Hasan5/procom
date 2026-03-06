f95690f20e2bdbf5cfd971ed84406b1823929437
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-06 19:41:59 +0500

    handling cors settings for frontend

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,11 +5,11 @@ import cors from "cors";
 const app = express();
 
 // Basic Configurations:
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(
 	express.urlencoded({
 		extended: true,
-		limit: "16kb",
+		limit: "5mb",
 	}),
 );
 //app.use(express.static("public"))
@@ -17,7 +17,7 @@ app.use(
 // CORS Configurations:
 app.use(
 	cors({
-		origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:8000",
+		origin: process.env.CORS_ORIGIN || "http://localhost:8000",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
