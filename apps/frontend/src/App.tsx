736a01e1ca68899c77ab3c417517e6054404e4da
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-07 19:39:01 +0500

    setup frontend with shadcn react router and redux

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1>Procom App</h1>
+      <Button variant={"ghost"}>Click me</Button>
+      <h1>Hasan</h1>
+    </>
+  )
+}
+
+export default App
